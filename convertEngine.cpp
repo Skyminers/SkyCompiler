bb3ec97177c40fafb138cb9337455cc5879116f6
Skyminer <458482582@qq.com>
2021-05-29 01:10:53 +0800

    modify convertEngine from LLVM-C to LLVM And create two system function : scan and print

--- convertEngine.cpp
+++ convertEngine.cpp
@@ -1,15 +1,16 @@
-////
-//// Created by 刘一辰 on 2021/5/20.
-////
 //
-//#include "convertEngine.h"
+// Created by 刘一辰 on 2021/5/20.
 //
-//ConvertEngine engine;
-//
-//// Unit test
-//int main() {
-//    engine.defineFunctionBegin();
-//    engine.printFunction();
-//    engine.defineFunctionEnd();
-//    engine.compile("test.out");
-//}
+
+#include "convertEngine.h"
+
+ConvertEngine engine;
+
+void ConvertEngine::compile() {
+    module->print(dbgs(), nullptr);
+}
+
+// Unit test
+int main() {
+    engine.compile();
+}
--- convertEngine.h
+++ convertEngine.h
@@ -1,123 +1,52 @@
-////
-//// Created by 刘一辰 on 2021/5/20.
-////
 //
-//#ifndef CONVERTENGINE_H
-//#define CONVERTENGINE_H
+// Created by 刘一辰 on 2021/5/20.
 //
-//#include <llvm-c/Core.h>
-//#include <llvm-c/ExecutionEngine.h>
-//#include <llvm-c/Target.h>
-//#include <llvm-c/Analysis.h>
-//#include <llvm-c/BitWriter.h>
-//#include <llvm/IR/Function.h>
-//
-//#include <cstdio>
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//class ConvertEngine{
-//private:
-//    LLVMModuleRef mod;
-//    LLVMBasicBlockRef entry;
-//    LLVMValueRef nowFunction;
-//    LLVMBuilderRef builder;
-//    bool returnFlag;
-//    vector<LLVMValueRef> funcValueList;
-//    LLVMValueRef printFunc;
-//
-//public:
-//    ConvertEngine(){
-//        mod = LLVMModuleCreateWithName("sky_module");
-//        builder = LLVMCreateBuilder();
-//        entry = nullptr;
-//        nowFunction = nullptr;
-////        std::vector<llvm::Type*> arg_types;
-////        arg_types.push_back(LLVMIntPtrType(LLVMInt8Type()));
-////        auto printf_type = llvm::FunctionType::get(LLVMInt32Type(), llvm::makeArrayRef(arg_types), true);
-////        auto func = llvm::Function::Create(printf_type, llvm::Function::ExternalLinkage, llvm::Twine("printf"), mod.get());
-//        printFunc = LLVMGetNamedFunction(mod, "printf");
-//    }
-//
-//    void newFunction(/*A careful module description is needed here.*/){
-//        // An a+b function is used to test.
-//
-//        // Create function block
-//        LLVMTypeRef params[] = { LLVMInt32Type(), LLVMInt32Type()};
-//        auto retType = LLVMFunctionType(LLVMInt32Type(), params, 2, 0);
-//        auto sum = LLVMAddFunction(mod, "sum", retType);
-//
-//        // Add function code
-//        auto entry = LLVMAppendBasicBlock(sum, "entry");
-//        auto builder = LLVMCreateBuilder();
-//        LLVMPositionBuilderAtEnd(builder, entry);
-//        auto tmp = LLVMBuildAdd(builder, LLVMGetParam(sum, 0), LLVMGetParam(sum, 1), "tmp");
-//        LLVMBuildRet(builder, tmp);
-//
-//        // Check code
-//        char *buf = nullptr;
-//        LLVMVerifyModule(mod, LLVMAbortProcessAction, &buf);
-//        LLVMDisposeMessage(buf);
-//    }
-//
-//    int printFunction(/*A description of output is needed*/){
-//        LLVMPositionBuilderAtEnd(builder, entry);
-//        LLVMValueRef a[] = { LLVMConstString("test", 4, false) };
-//        auto tmp = LLVMBuildCall(builder, printFunc, a, 1, "tmp");
-//        LLVMBuildRet(builder, tmp);
-//    }
-//
-//    /*
-//     * Create function
-//     * 1. Call defineFunctionBegin()
-//     * 2. Call right function to create basic code
-//     * 3. Call defineFunctionEnd()
-//     */
-//
-//    int defineFunctionBegin(/*A careful module description is needed here.*/){
-//        if(nowFunction){
-//            cerr << "[error] Can not define function in function" << endl;
-//            return 1;
-//        }
-//        LLVMTypeRef params[] = { LLVMInt32Type(), LLVMInt32Type()}; // function params
-//        auto retType = LLVMFunctionType(LLVMInt32Type(), params, 2, 0); // function return type
-//        nowFunction = LLVMAddFunction(mod, "sum", retType); // function created
-//        entry = LLVMAppendBasicBlock(nowFunction, "entry");
-//        returnFlag = false;
-//        funcValueList.clear();
-//    }
-//
-//    int defineFunctionEnd(){
-//        if(!nowFunction){
-//            cerr << "[error] You should call defineFunctionBegin() before call defineFunctionEnd()" << endl;
-//            return 1;
-//        }
-//        if(!entry){
-//            cerr << "[error] Empty function is not allowed" << endl;
-//            return 2;
-//        }
-//
-//        if(!returnFlag){
-//            cerr << "[error] Return can not be ignored" << endl;
-//            return 3;
-//        }
-//        nowFunction = nullptr;
-//        return 0;
-//    }
-//
-//    int compile(string filename){
-//        if(!filename.empty()) {
-//            filename = "sky.out";
-//        }
-//        if(LLVMWriteBitcodeToFile(mod, filename.c_str())){
-//            cerr << "[error] An error occurs at writing" << endl;
-//            return 1;
-//        }
-//        cout << "Compile successfully to " << filename << endl;
-//        return 0;
-//    }
-//};
-//
-//#endif //CONVERTENGINE_H
+
+#ifndef CONVERTENGINE_H
+#define CONVERTENGINE_H
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Target/TargetMachine.h>
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace llvm;
+static LLVMContext context;
+static IRBuilder<> builder(context);
+
+class ConvertEngine{
+private:
+    Module *module;
+    uint layoutID;
+    Function *scan, *print;
+    Function *main;
+public:
+    ConvertEngine(){
+        module = new Module("skyModule", context);
+        layoutID = module->getDataLayout().getAllocaAddrSpace();
+
+        // scan function
+        auto funcType = FunctionType::get(builder.getInt32Ty(), true);
+        scan = Function::Create(funcType, Function::ExternalLinkage, Twine("scanf"), module);
+        scan->setCallingConv(CallingConv::C);
+
+        // print function
+        vector<Type*> valuesType;
+        valuesType.push_back(builder.getInt8PtrTy());
+        funcType = FunctionType::get(builder.getInt32Ty(), makeArrayRef(valuesType), true);
+        print = Function::Create(funcType, Function::ExternalLinkage, Twine("printf"), module);
+        print->setCallingConv(CallingConv::C);
+    }
+
+
+    void compile();
+};
+
+#endif //CONVERTENGINE_H
